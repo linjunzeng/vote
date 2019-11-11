ac15e6006c6f84cc613e0cddc717bd649ba5e487
NickName <838699083@qq.com>
2019-11-11 16:27:42 +0800

    我的投票

--- pages/my/my.js
+++ pages/my/my.js
@@ -2,13 +2,42 @@ import { getUserVote } from '../../utils/api.js'
 let app = getApp();
 Page({
   data: {
+    title:[
+      {
+        'title':'我发起的',
+        'returnObject':'createVote'
+      },
+      {
+        'title': '我参与的',
+        'returnObject': 'joinVote'
+      }
+    ],
+    showdata:[],
+    returnObject:{}
   },
   onLoad: function (options) {
     getUserVote(app.userId)
     .then(res => {
       console.log(res)
+      this.setData({
+        returnObject: res.returnObject,
+        showdata: res.returnObject.createVote
+      })
     }).catch(err =>{
       app.showToast(err.message)
+    })
+    
+  },
+  towhere: function (event) {
+    let _id = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/vote/voteShow?tid=' + _id
+    })
+  },
+  tab: function (event){
+    let kaa = event.currentTarget.dataset.id;
+    this.setData({
+      showdata: this.data.returnObject[kaa]
     })
   }
 })
